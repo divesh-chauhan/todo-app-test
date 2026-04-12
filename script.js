61a1562b4d4f31f32f2f2b7140f75b66f9a6192e
divesh chauhan <diveshc207@gmail.com>
2026-04-12 20:35:43 +0530

    feat: add render function for page rendering

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function saveTask(tasks){
 function getTask(){
     return JSON.parse(localStorage.getItem("tasks"))
 }
+
+
+//* render 
+function render(){
+    const tasks = getTask()
+    ul.innerHTML = ""
+
+    tasks.forEach(task => {
+        const li = document.createElement("li")
+
+        li.innerHTML = `
+        <span>${task.title}</span>
+        <button id="edit-task" data-id="${task.id}">Edit</button>
+        <button id="delete-task" data-id="${task.id}">Delete</button>
+        `
+        ul.appendChild(li)
+    })
+}
